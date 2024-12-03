0de97c4c7a0c307f92d2732bd3052aa8ee4d1e9f
yamada3-1990 <k.ae.de19900220+1@gmail.com>
2024-12-03 20:39:26 +0900

    moon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import './home.css';
 export default function Home() {
   return (
     <div className="scene-container">
-      <Spline scene="https://prod.spline.design/DWxGheeT0DbekvSc/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/SztTXctKGIwcdknb/scene.splinecode" />
       {/* <div className="overlay">
         <h1>Home Page</h1>
         <p>Welcome to the Spline scene! Explore the 3D world below.</p>
