6cb0aa8877776f1eeb64032211717b9416699d88
yamada3-1990 <k.ae.de19900220+1@gmail.com>
2024-12-17 21:15:54 +0900

    build

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,85 @@
-// import React from 'react';
+import React, { useState } from 'react';
 import './about.css';
 
 export default function About() {
+  // 言語状態の管理
+  const [language, setLanguage] = useState('en');
+
+
+
+  // 言語ごとのテキスト
+  const texts = {
+    en: {
+      title: 'About Page',
+      description: 'This is the about section. Learn more about our project!',
+      name: 'Name:',
+      skill: 'Skill:',
+      skl1: 'python',
+      skl2: 'blender',
+      qualification: 'Qualification etc.:',
+      hobby: 'Hobby:',
+      links: 'Links:',
+    },
+    ja: {
+      title: '概要ページ',
+      description: 'これは概要セクションです。プロジェクトについて詳しく学びましょう！',
+      name: '名前:',
+      skill: 'スキル:',
+      skl1: 'python',
+      skl2: 'blender',
+      qualification: '資格など:',
+      hobby: '趣味:',
+      links: 'リンク:',
+    },
+  };
+
+  texts.en.skills = ['python', 'blender', 'React', 'JavaScript', 'Spline', 'C', 'HTML/CSS', 'Processing', 'Java', 'GitHub', 'Go'];
+  texts.ja.skills = ['python', 'blender', 'React', 'JavaScript', 'Spline', 'C', 'HTML/CSS', 'Processing', 'Java', 'GitHub', 'Go'];
+
+  texts.en.qualifications = ['TOEIC 830', 'Hazardous Materials Engineer in Class B Group 4', 'EIKEN grade 2', 'FE(Fundamental Information Technology Engineer Examination)', 'GCI 2023 Summer'];
+  texts.ja.qualifications = ['TOEIC 830', '危険物取扱者乙種第4類', '英検2級', '基本情報技術者試験', 'GCI 2023 Summer'];
+  // 特定の言語に切り替える関数
+  const switchToJa = () => setLanguage('ja');
+  const switchToEn = () => setLanguage('en');
+
   return (
+
     <div className="scene-container">
       <div className="overlay">
-        <h1>About Page</h1>
-        <p>This is the about section. Learn more about our project!</p>
+        <button onClick={switchToJa} disabled={language === 'ja'}>
+          JA
+        </button>
+        <button onClick={switchToEn} disabled={language === 'en'}>
+          EN
+        </button>
+        <h1>{texts[language].title}</h1>
+        <p>{texts[language].description}</p>
       </div>
+
+      <p>{texts[language].name}</p>
+
+      {/* スキル */}
+      <div className="skills-container">
+        <p>{texts[language].skill}</p>
+        <ul className="skills-list">
+          {texts[language].skills.map((skill, index) => (
+            <li key={index} className="skill-item">{skill}</li>
+          ))}
+        </ul>
+      </div>
+
+      {/* 資格 */}
+      <div className="skills-container">
+        <p>{texts[language].qualification}</p>
+        <ul className="skills-list">
+          {texts[language].qualifications.map((skill, index) => (
+            <li key={index} className="skill-item">{skill}</li>
+          ))}
+        </ul>
+      </div>
+
+      <p>{texts[language].hobby}</p>
+      <p>{texts[language].links}</p>
     </div>
   );
 }
