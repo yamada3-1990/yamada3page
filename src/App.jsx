852bea68346379549505cb76229edc95be8a44f9
yamada3-1990 <k.ae.de19900220+1@gmail.com>
2024-12-19 21:39:16 +0900

    delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,4 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './App.css';
-
-// 各ページコンポーネントをインポート
-import Home from './pages/Home';
-import About from './pages/About';
-// import Contact from './pages/Links';
-
-export default function App() {
-  return (
-    <Router basename="/yamada3page">
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About me</Link></li>
-          {/* <li><Link to="/links">Links</Link></li> */}
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        {/* <Route path="/links" element={<Contact />} /> */}
-      </Routes>
-    </Router>
-
-  );
-}
-
-
-
-// テスト環境用
-
-// // import React from 'react';
+// import React from 'react';
 // import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 // import './App.css';
 
@@ -42,7 +9,7 @@ export default function App() {
 
 // export default function App() {
 //   return (
-//     <Router>
+//     <Router basename="/yamada3page">
 //       <nav>
 //         <ul>
 //           <li><Link to="/">Home</Link></li>
@@ -59,3 +26,36 @@ export default function App() {
 
 //   );
 // }
+
+
+
+// テスト環境用
+
+// import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+
+// 各ページコンポーネントをインポート
+import Home from './pages/Home';
+import About from './pages/About';
+// import Contact from './pages/Links';
+
+export default function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About me</Link></li>
+          {/* <li><Link to="/links">Links</Link></li> */}
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/links" element={<Contact />} /> */}
+      </Routes>
+    </Router>
+
+  );
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,7 @@ import './about.css';
 export default function About() {
   // 言語状態の管理
   const [language, setLanguage] = useState('en');
-
-
+  const [isChecked, setIsChecked] = useState(false); // チェック状態を管理
 
   // 言語ごとのテキスト
   const texts = {
@@ -19,6 +18,8 @@ export default function About() {
       qualification: 'Qualification etc.:',
       hobby: 'Hobby:',
       links: 'Links:',
+      skills: ['python', 'blender', 'React', 'JavaScript', 'Spline', 'C', 'HTML/CSS', 'Processing', 'Java', 'GitHub', 'Go', 'Vite'],
+      qualifications: ['TOEIC 830', 'Hazardous Materials Engineer in Class B Group 4', 'EIKEN grade 2', 'FE(Fundamental Information Technology Engineer Examination)', 'GCI 2023 Summer'],
     },
     ja: {
       title: '概要ページ',
@@ -30,36 +31,27 @@ export default function About() {
       qualification: '資格など:',
       hobby: '趣味:',
       links: 'リンク:',
+      skills: ['python', 'blender', 'React', 'JavaScript', 'Spline', 'C', 'HTML/CSS', 'Processing', 'Java', 'GitHub', 'Go', 'Vite'],
+      qualifications: ['TOEIC 830', '危険物取扱者乙種第4類', '英検2級', '基本情報技術者試験', 'GCI 2023 Summer'],
     },
   };
 
-  texts.en.skills = ['python', 'blender', 'React', 'JavaScript', 'Spline', 'C', 'HTML/CSS', 'Processing', 'Java', 'GitHub', 'Go', 'Vite'];
-  texts.ja.skills = ['python', 'blender', 'React', 'JavaScript', 'Spline', 'C', 'HTML/CSS', 'Processing', 'Java', 'GitHub', 'Go', 'Vite'];
-
-  texts.en.qualifications = ['TOEIC 830', 'Hazardous Materials Engineer in Class B Group 4', 'EIKEN grade 2', 'FE(Fundamental Information Technology Engineer Examination)', 'GCI 2023 Summer'];
-  texts.ja.qualifications = ['TOEIC 830', '危険物取扱者乙種第4類', '英検2級', '基本情報技術者試験', 'GCI 2023 Summer'];
-  // 特定の言語に切り替える関数
-  const switchToJa = () => setLanguage('ja');
-  const switchToEn = () => setLanguage('en');
+  // トグルスイッチを操作する関数
+  const handleToggle = () => {
+    setIsChecked(!isChecked);
+    setLanguage(isChecked ? 'en' : 'ja'); // チェック状態に応じて言語を切り替え
+  };
 
   return (
-    
     <div className="container">
-      <br></br>
-      <br></br>
-      <br></br>
-      <div >
-        <button onClick={switchToJa} disabled={language === 'ja'}>
-          JA
-        </button>
-        <button onClick={switchToEn} disabled={language === 'en'}>
-          EN
-        </button>
-        <h1>{texts[language].title}</h1>
-        <p>{texts[language].description}</p>
+      <div className="fixed-button">
+        <label className="toggle-button-4">
+          <input type="checkbox" checked={isChecked} onChange={handleToggle} />
+        </label>
       </div>
-
-      <p>{texts[language].name}</p>
+      <div className='skills-container'>
+        <p>yamada3</p>
+      </div>
 
       {/* スキル */}
       <div className="skills-container">
@@ -68,7 +60,6 @@ export default function About() {
           {texts[language].skills.map((skill, index) => (
             <li key={index} className="skill-item">{skill}</li>
           ))}
-          {/* <span className="skill-icons--blender-dark"></span> */}
         </ul>
       </div>
 
@@ -82,8 +73,12 @@ export default function About() {
         </ul>
       </div>
 
-      <p>{texts[language].hobby}</p>
-      <p>{texts[language].links}</p>
+      <div className='skills-container'>
+        <p>{texts[language].hobby}</p>
+      </div>
+      <div className='skills-container'>
+        <p>{texts[language].links}</p>
+      </div>
     </div>
   );
 }
