5a6777b615d280cef1f084aad6af883e9a091466
yamada3-1990 <k.ae.de19900220+1@gmail.com>
2025-02-09 10:31:49 +0900

    いろいろ変えた

---
+++ js/header.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("/pages/header.html") 
+        .then(response => response.text()) 
+        .then(data => {
+            document.getElementById("header-container").innerHTML = data; // 挿入
+        })
+        .catch(error => console.error("ヘッダーの読み込みに失敗しました:", error));
+});
