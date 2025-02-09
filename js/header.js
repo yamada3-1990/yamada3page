8d08ded0fb939363431195695b660bc48e7754b6
yamada3-1990 <k.ae.de19900220+1@gmail.com>
2025-02-09 10:45:50 +0900

    ディレクトリ構成変えた

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("/header.html") 
+    fetch("/pages/header.html") 
         .then(response => response.text()) 
         .then(data => {
             document.getElementById("header-container").innerHTML = data; // 挿入
