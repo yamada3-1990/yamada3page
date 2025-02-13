d258af82e1727104419982dd8a0de98ee2c84cd7
yamada3-1990 <k.ae.de19900220+1@gmail.com>
2025-02-13 14:14:38 +0900

    変えた

--- js/header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("/header/index.html") 
-        .then(response => response.text()) 
-        .then(data => {
-            document.getElementById("header-container").innerHTML = data; // 挿入
-        })
-        .catch(error => console.error("ヘッダーの読み込みに失敗しました:", error));
-});
